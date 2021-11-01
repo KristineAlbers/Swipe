8f5f40a40472e0cb16c23bc0ce928bb030e3ed58
KristineAlbers <1259@rts-365.dk>
2021-11-01 15:07:59 +0100

    swipe

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let jokeItem = document.querySelector('.jokeItem');
 let touchCoordinatesStart;
 let touchCoordinatesMove;
+let touchend
+let deleteButtonWidth = (window.screen.width * 40) / 100;
+
 
 jokeItem.addEventListener('touchstart', (e) => {
     touchCoordinatesStart = e.touches[0].clientX;
@@ -9,9 +12,22 @@ jokeItem.addEventListener('touchstart', (e) => {
 
 jokeItem.addEventListener('touchmove', (e) => {
     touchCoordinatesMove = Math.floor(e.touches[0].clientX);
-    if (touchCoordinatesMove < touchCoordinatesStart) {
+    if (
+        touchCoordinatesMove < touchCoordinatesStart &&
+        touchCoordinatesMove > touchCoordinatesStart - deleteButtonWidth
+        ) {
         //console.log(e.touches[0].clientX);
         jokeItem.style.transform = `translateX(${
             touchCoordinatesMove -touchCoordinatesStart
         }px)`;}
+});
+
+jokeItem.addEventListener('touchend', (e) => {
+    touchend = Math.floor(e.changedTouches[0].clientX);
+    if (touchend < touchCoordinatesStart - deleteButtonWidth / 2) {
+        //console.log('test')
+        jokeItem.style.transform = `translateX(-${deleteButtonWidth}px)`;
+    } else {
+        jokeItem.style.transform = `translateX(${e.target.offsetLeft})`;
+    }
 });
